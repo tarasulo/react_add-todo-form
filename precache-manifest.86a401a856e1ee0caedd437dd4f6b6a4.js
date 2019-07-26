e32f3c29b023ad556a3b21960ebfe8e85c00f26d
Taras Khalak <tarasulo@gmail.com>
2019-07-26 07:36:52 +0300

    Updates

---
+++ precache-manifest.86a401a856e1ee0caedd437dd4f6b6a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1dceb6b35df654af86a10f8f71db48",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "1406c749c4240718feac",
+    "url": "/react_add-todo-form/static/css/main.4d6d0c77.chunk.css"
+  },
+  {
+    "revision": "1b8596f2d357faafcb17",
+    "url": "/react_add-todo-form/static/js/2.74dd4a25.chunk.js"
+  },
+  {
+    "revision": "1406c749c4240718feac",
+    "url": "/react_add-todo-form/static/js/main.e3a7e75e.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
